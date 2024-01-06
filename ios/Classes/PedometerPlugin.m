1eba6eb8b85ab49367a82b8526cc28e15b133980
akaboshinit <akaboshinit@gmail.com>
2024-01-07 00:13:23 +0900

    fix ios setting file

--- ios/Classes/PedometerPlugin.m
+++ ios/Classes/PedometerPlugin.m
@@ -1,11 +1,11 @@
 #import "PedometerPlugin.h"
-#if __has_include(<pedometer/pedometer-Swift.h>)
-#import <pedometer/pedometer-Swift.h>
+#if __has_include(<pedometer_plus/pedometer_plus-Swift.h>)
+#import <pedometer_plus/pedometer_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "pedometer-Swift.h"
+#import "pedometer_plus-Swift.h"
 #endif
 
 @implementation PedometerPlugin
